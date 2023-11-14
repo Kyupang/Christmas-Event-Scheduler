95179fafd684d01193615c7e968ee9262bbe22da
KimKyuHwan <vkdls754@gmail.com>
2023-11-14 19:56:43 +0900

    feat(OrderParser): 파서 클래스 구현코드 추가

---
+++ src/main/java/christmas/util/OrderParser.java
@@ -0,0 +1,22 @@
+package christmas.util;
+
+import christmas.domain.Order;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderParser {
+    public static List<Order> parseOrders(String menuAndQuantityInput) {
+        List<Order> orders = new ArrayList<>();
+        String[] orderTokens = menuAndQuantityInput.split(",");
+        for (String orderToken : orderTokens) {
+            String[] parts = orderToken.split("-");
+            if (parts.length == 2) {
+                String menu = parts[0].trim();
+                int quantity = Integer.parseInt(parts[1].trim());
+                Order order = new Order(menu, quantity);
+                orders.add(order);
+            }
+        }
+        return orders;
+    }
+}
